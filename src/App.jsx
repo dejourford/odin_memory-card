f9b6d0867fa6b00d897b8a85fcc084f9d6862323
De'Jour <dejourford@yahoo.com>
2026-04-15 21:43:25 -0500

    get card data from api

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,39 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import { Header } from './components/Header'
 import { Card } from './components/Card'
 
 function App() {
+  const [cardData, setCardData] = useState([]);
+
   
+  const apiAddress = "https://pokeapi.co/api/v2/pokemon/"
+  const apiImgPath = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(apiAddress)
+      const data = await response.json();
+      console.log('reults:', data.results)
+      setCardData(data.results);
+    }
+
+    fetchData();
+  }, []);
+
+  console.log("cardData:", cardData.results)
 
   return (
     <>
     <Header />
+    <section className='card-grid'>
+        {cardData.map((card, index) => (
+          <div className='card'>
+            <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`} alt={card.name} />
+            <p>{card.name}</p>
+          </div>
+        ))}
+    </section>
     </>
   )
 }
