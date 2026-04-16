a1100767399294a1b32529059510f48a136e4156
De'Jour <dejourford@yahoo.com>
2026-04-15 22:29:43 -0500

    use .some method in click handler to update score or return if card already clicked

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,9 @@ function App() {
   const [cardData, setCardData] = useState([]);
   const [score, setScore] = useState(0);
   const [bestScore, setBestScore] = useState(0);
+  const [collection, setCollection] = useState([]);
 
-  
+  // fetch data
   const apiAddress = "https://pokeapi.co/api/v2/pokemon?limit=10"
 
   useEffect(() => {
@@ -21,17 +22,36 @@ function App() {
     fetchData();
   }, []);
 
+  // handle card click
+  const handleCardClick = (name) => {
+    if (collection.some((item) => item === name)) {
+      console.log("return please");
+      if (score > bestScore) {
+        setBestScore(score)
+
+      }
+      setScore(0)
+      setCollection([])
+      return
+    }
+
+    setCollection(prev => [...prev, name]);
+
+    console.log(collection)
+    setScore(prevScore => prevScore + 1)
+  }
+
   return (
     <>
-    <Header score={score} bestScore={bestScore} />
-    <section className='card-grid'>
+      <Header score={score} bestScore={bestScore} />
+      <section className='card-grid'>
         {cardData.map((card, index) => (
-          <div className='card' key={card.name}>
+          <div className='card' key={card.name} onClick={() => handleCardClick(card.name)}>
             <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`} alt={card.name} />
             <p>{card.name}</p>
           </div>
         ))}
-    </section>
+      </section>
     </>
   )
 }
