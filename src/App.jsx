68b6f7afbb9aabc2df12d2ed0157de69b0e0e3c7
De'Jour <dejourford@yahoo.com>
2026-04-16 21:46:24 -0500

    randomize cards on each click

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
   const [bestScore, setBestScore] = useState(0);
   const [collection, setCollection] = useState([]);
 
+
   // fetch data
   const apiAddress = "https://pokeapi.co/api/v2/pokemon?limit=10"
 
@@ -17,22 +18,37 @@ function App() {
       const response = await fetch(apiAddress)
       const data = await response.json();
       setCardData(data.results);
+      console.log('data:', data.results)
     }
 
     fetchData();
   }, []);
 
+  // function to shuffle cards
+  const shuffleCards = (array) => {
+    const shuffled = [...array];
+
+    for (let i = shuffled.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+
+      // swap
+      [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+    }
+    return shuffled;
+  }
+
   // handle card click
   const handleCardClick = (name) => {
+
     if (collection.length === cardData.length) {
       alert("You have won the game!")
       return
     }
-    
+
     if (collection.some((item) => item === name)) {
       console.log("return please");
       if (score > bestScore) {
-        setBestScore(score)
+        setBestScore(prev => Math.max(prev, score));
 
       }
       setScore(0)
@@ -41,7 +57,7 @@ function App() {
     }
 
     setCollection(prev => [...prev, name]);
-
+    setCardData(prev => shuffleCards(prev))
     console.log(collection)
     setScore(prevScore => prevScore + 1)
   }
@@ -50,12 +66,19 @@ function App() {
     <>
       <Header score={score} bestScore={bestScore} />
       <section className='card-grid'>
-        {cardData.map((card, index) => (
-          <div className='card' key={card.name} onClick={() => handleCardClick(card.name)}>
-            <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`} alt={card.name} />
-            <p>{card.name}</p>
-          </div>
-        ))}
+        {cardData.map((card) => {
+          const id = card.url.split("/").filter(Boolean).pop();
+
+          return (
+            <div className='card' key={card.name} onClick={() => handleCardClick(card.name)}>
+              <img
+                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`}
+                alt={card.name}
+              />
+              <p>{card.name}</p>
+            </div>
+          );
+        })}
       </section>
     </>
   )
